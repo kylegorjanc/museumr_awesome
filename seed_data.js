7b9d45595f3b90ebb4bc15ee21b65c4cf843582f
shishkina <shishkina.kate@gmail.com>
2015-11-19 15:57:20 -0500

    added seed_data.js

---
+++ seed_data.js
@@ -0,0 +1,35 @@
+### Seed the database with the following information
+
+#### Artists:
+- Pablo Picasso (1881, Spain)
+- Vincent Van Gogh (1853, Netherlands)
+- Jean-Michel Basquiat (1960, USA)
+
+#### Artist photos:
+  - http://i.telegraph.co.uk/multimedia/archive/01773/pablo-picasso_1773978b.jpg
+  - http://www.artcyclopedia.org/art/van-gogh-self.jpg
+  - http://urbanepop.files.wordpress.com/2012/08/jean_michel_basquiat-hr1.jpg
+
+#### Artist bios:
+  - Pablo Ruiz y Picasso, also known as Pablo Picasso, was a Spanish painter, sculptor, printmaker, ceramicist, stage designer, poet and playwright who spent most of his adult life in France.
+  - Vincent Willem van Gogh was a Post-Impressionist painter of Dutch origin whose work—notable for its rough beauty, emotional honesty, and bold color—had a far-reaching influence on 20th-century art.
+  - Jean-Michel Basquiat was an American artist. Basquiat first achieved notoriety as part of SAMO, an informal graffiti group who wrote enigmatic epigrams in the cultural hotbed of the Lower East Side of Manhattan, New York City during the late 1970s where the hip hop, post-punk and street art movements had coalesced.
+
+
+#### Paintings:
+##### Picasso
+  - Guernica (1937), Les Demoiselles d'Avignon (1907)
+
+##### Van Gogh
+  - The Starry Night (1889), Cafe Terrace at Night (1888)
+
+##### Basquiat
+  - Irony of Negro Policeman (1981), Hollywood Africans (1983)
+
+#### Painting photos:
+- http://2.bp.blogspot.com/-dlllLhl8c7s/UnrFUvtgMxI/AAAAAAAARXQ/Cv8QKBgfVD4/s1600/guernica.jpg
+- http://www.moma.org/explore/conservation/demoiselles/images/demoiselles_NewFINAL.jpg
+- http://atomictoasters.com/wp-content/gallery/professors-shutdown-images/starry-night.jpg
+- http://upload.wikimedia.org/wikipedia/commons/0/09/Van_Gogh_-_Terrasse_des_Caf%C3%A9s_an_der_Place_du_Forum_in_Arles_am_Abend1.jpeg
+- http://uploads7.wikiart.org/images/jean-michel-basquiat/ironew-york-of-the-negro-policeman.jpg!Blog.jpg
+- http://uploads0.wikiart.org/images/jean-michel-basquiat/hollywood-africans.jpg
